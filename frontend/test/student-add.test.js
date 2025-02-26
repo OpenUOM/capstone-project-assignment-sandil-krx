db754a9bf0efe659bab3a141d5b1fcd10de5d8cb
Sandil <it23657700@my.sliit.lk>
2025-02-26 14:05:48 +0530

    update the url

--- frontend/test/student-add.test.js
+++ frontend/test/student-add.test.js
@@ -2,13 +2,13 @@ import { Selector } from 'testcafe';
 process.env.NODE_ENV = "test";
 
 fixture`Testing Student UI`
-    .page`http://localhost:4401/student`
+    .page`http://localhost:4401/addStudent`
 
 test('Testing add students', async t => {
 
     //await t.navigateTo("/dbinitialize");
 
-    await t.navigateTo("/addStudent");
+    //await t.navigateTo("/addStudent");
     await t.typeText("#student-id", "999999");
     await t.typeText("#student-name", "Pasindu Basnayaka");
     await t.typeText("#student-age", "45");
